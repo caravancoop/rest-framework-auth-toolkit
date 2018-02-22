3346a321a487524f571ec19d4817b9e19f14da8f
Éric Araujo <earaujo@caravan.coop>
2018-02-21 19:58:59 -0500

    add base user model with email as username

---
+++ rest_auth_toolkit/admin.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseEmailUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    # add_form_template = None
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
