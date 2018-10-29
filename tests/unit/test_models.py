8306e0a395e5b133da8bfd2251b04cf75078b2d1
Julien Labonté <julien@caravan.coop>
2018-10-29 15:14:00 -0400

    add unit tests for base user model

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,2 +1,10 @@
 def test_baseemailuser_str(user0):
     assert str(user0) == 'bob@example.com'
+
+
+def test_baseemailuser_get_short_name(user0):
+    assert user0.get_short_name() == 'bob@example.com'
+
+
+def test_baseemailuser_natural_key(user0):
+    assert user0.natural_key() == 'bob@example.com'
