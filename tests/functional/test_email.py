353391412d27f0aef6bcd597bdd5c625ebfacd98
JulienLabonte <julien@caravan.coop>
2018-10-30 15:24:27 -0400

    Add functional tests for sign up

--- tests/functional/test_email.py
+++ tests/functional/test_email.py
@@ -4,3 +4,17 @@ from django.urls import reverse
 def test_signup(db, django_app):
     params = {"email": "zack@example.com", "password": "correct battery horse staple"}
     django_app.post_json(reverse("auth:signup"), params=params, status=201)
+
+
+def test_signup_same_as_password(db, django_app):
+    params = {"email": "bobby@example.com", "password": "bobby@example.com"}
+    resp = django_app.post_json(reverse("auth:signup"), params=params, status=400)
+
+    assert 'password' in resp.json
+
+
+def test_signup_invalid_email(db, django_app):
+    params = {"email": "bobby", "password": "bobby@example.com"}
+    resp = django_app.post_json(reverse("auth:signup"), params=params, status=400)
+
+    assert 'email' in resp.json
