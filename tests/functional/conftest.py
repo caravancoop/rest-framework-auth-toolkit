91fb85e2fb63bc5b00b8d522852859af4d6375e8
JulienLabonte <julien@caravan.coop>
2018-11-05 14:52:50 -0500

    add test for facebook login

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -13,7 +13,25 @@ def user0(db):
 
 
 @pytest.fixture
+def userfb0(db):
+    return User.objects.create_user(
+        email='alex@example.com',
+        first_name='Alex',
+        last_name='Facebook',
+        facebook_id='1234dddd',
+        facebook_access_token='oauthtoken1111',
+        is_active=True,
+    )
+
+
+@pytest.fixture
 def token0(user0):
     return APIToken.objects.create_token(
         user=user0,
+    )
+
+@pytest.fixture
+def token1(userfb0):
+    return APIToken.objects.create_token(
+        user=userfb0,
     )
