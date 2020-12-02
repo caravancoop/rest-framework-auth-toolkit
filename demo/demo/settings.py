eeb496b5bb2d1ef27ac0353a2632ba673c200452
Éric Araujo <earaujo@caravan.coop>
2020-12-02 18:11:00 -0500

    update dependencies and ci config

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -116,6 +116,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 
